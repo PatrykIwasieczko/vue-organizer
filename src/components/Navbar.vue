77840c0ba102b742a51d5ca27a81ee136698e219
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-02 12:30:36 +0200

    Create and import basics of toolbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <v-toolbar app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Orga</span>
+        <span>nizer</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+
+<script>
+export default {};
+</script>
