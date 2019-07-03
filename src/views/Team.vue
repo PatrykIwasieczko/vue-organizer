60d403a164f1887a2c39bfba54615518290bcced
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-03 16:59:36 +0200

    Create avatars

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,7 +6,11 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card class="text-xs-center ma-3">
-            <v-responsive class="pt-4">image here</v-responsive>
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar" />
+              </v-avatar>
+            </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
               <div class="grey--text">{{ person.role }}</div>
