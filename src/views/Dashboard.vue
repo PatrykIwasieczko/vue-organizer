26a227c03fd66e026645f0b763d126a748a91475
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-03 11:46:38 +0200

    Add dummy data to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,23 +2,23 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-card flat class="pa-3">
+      <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
         <v-layout row wrap>
           <v-flex xs12 md6>
             <div class="caption grey--text">Project title</div>
-            <div>Create a new project</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>Patryk Iwasieczko</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due date</div>
-            <div>1st Jan 2077</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{ project.status }}</div>
           </v-flex>
         </v-layout>
       </v-card>
@@ -27,5 +27,44 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "Patryk Iwasieczko",
+          due: "1st Jan 2077",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus ipsam ullam amet. Maiores dolorum quam ea cumque voluptate harum nihil distinctio adipisci molestias quis alias odit, molestiae sint porro perferendis?"
+        },
+        {
+          title: "Find a job",
+          person: "Patryk Iwasieczko",
+          due: "1st Jan 2020",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt veniam modi totam quo reiciendis. Eum odio beatae voluptates ullam. Commodi maiores vel explicabo quidem odit quam reiciendis iste, dolore deserunt!"
+        },
+        {
+          title: "Start learning Vue",
+          person: "Patryk Iwasieczko",
+          due: "20th Sep 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, sunt excepturi. Deleniti odio et dolorum necessitatibus labore a modi. Hic tempore dicta modi? Quas dolor similique tempore at ipsa veniam."
+        },
+        {
+          title: "Hire new members to our developers team",
+          person: "John Doe",
+          due: "1st Jan 2020",
+          status: "to do",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla rem perferendis atque odit autem repudiandae debitis molestias quasi eaque, error nemo optio non quis explicabo accusantium distinctio consequatur, commodi amet?"
+        }
+      ]
+    };
+  }
+};
 </script>
