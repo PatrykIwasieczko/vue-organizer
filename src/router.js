49f34bf9612155d46392a63348e4b39cabcb9d38
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-04 20:38:40 +0200

    Add basics of login component

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Dashboard from "./views/Dashboard.vue";
 import Projects from "./views/Projects.vue";
 import Team from "./views/Team.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
             path: "/team",
             name: "team",
             component: Team
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login
         }
     ]
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <v-container class="my-3">
+      <v-layout justify-center row class="mb-3">
+        <v-flex s12 md4>
+          <v-card class="pa-5">
+            <h3>Sign In</h3>
+            <v-form>
+              <v-text-field label="Email"></v-text-field>
+              <v-text-field label="Password"></v-text-field>
+            </v-form>
+            <v-btn flat class="success mx-0 my-4">Login</v-btn>
+            <p>You don't have an account? You can create one</p>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style>
+</style>
