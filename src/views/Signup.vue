beaca0bbbd0af062bfb483bc92da375fd5e52e55
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-05 12:35:39 +0200

    Add account creation functionality

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,10 +6,10 @@
           <v-card class="pa-5">
             <h3>Create a new account</h3>
             <v-form>
-              <v-text-field label="Email"></v-text-field>
-              <v-text-field label="Password"></v-text-field>
+              <v-text-field label="Email" v-model="email"></v-text-field>
+              <v-text-field label="Password" v-model="password"></v-text-field>
             </v-form>
-            <v-btn flat class="success mx-0 my-4">Sign Up</v-btn>
+            <v-btn flat class="success mx-0 my-4" @click="signup">Sign Up</v-btn>
             <v-spacer></v-spacer>
             <span>
               If you already have an account -
@@ -22,12 +22,30 @@
   </div>
 </template>
 <script>
+import firebase from "firebase";
 export default {
   name: "signup",
   data() {
-    return {};
+    return {
+      email: "",
+      password: ""
+    };
   },
-  methods: {}
+  methods: {
+    signup: function() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          function() {
+            alert("Your account has been created");
+          },
+          function(err) {
+            alert("Oops." + err.message);
+          }
+        );
+    }
+  }
 };
 </script>
 <style>
