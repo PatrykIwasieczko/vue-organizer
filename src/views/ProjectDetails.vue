972360b063f50e528fd75c27a487405063393208
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-07-17 21:16:43 +0200

    Add moreproject details

--- src/views/ProjectDetails.vue
+++ src/views/ProjectDetails.vue
@@ -2,12 +2,13 @@
   <div class="projects">
     <h1 class="subheading grey--text">Project details</h1>
     <v-container class="my-5">
-      <div class="project"></div>
-      <div class="header">{{ project.title }}</div>
       <v-card>
-        <v-card-text class="grey--text">
-          <div class="font-weight-bold">Due date: {{ project.due }}</div>
-          <div>{{ project.content }}</div>
+        <v-card-text>
+          <div class="font-weight-bold mb-3">{{ project.title }}</div>
+          <div>Created by: {{ project.person }}</div>
+          <div>Status: {{ project.status }}</div>
+          <div>Due date: {{ project.due }}</div>
+          <div class="mt-3">{{ project.content }}</div>
         </v-card-text>
       </v-card>
     </v-container>
