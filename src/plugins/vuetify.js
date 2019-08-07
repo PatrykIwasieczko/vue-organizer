4f221de4f923d79d0a3839626e9664af6972e67a
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-08-07 17:47:50 +0200

    Fix theme colours

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,12 +7,16 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     icons: {
-        iconfont: "md",
-        theme: {
-            primary: "#9652ff",
-            success: "#3cd1c2",
-            info: "#ffaa2c",
-            error: "#f83e70"
+        iconfont: "md"
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: "#9652ff",
+                success: "#3cd1c2",
+                info: "#ffaa2c",
+                error: "#f83e70"
+            }
         }
     }
 });
