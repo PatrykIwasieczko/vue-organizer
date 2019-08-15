e1a251149281bc65c41785cf1ce74261aa941471
patryk-iwasieczko <patryk.iwasieczko@gmail.com>
2019-08-15 21:55:55 +0200

    Style and improve editProject functionality

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -4,10 +4,29 @@
     <v-container class="my-5">
       <v-card>
         <v-card-text>
-          <v-text-field v-model="project.title" label="Project title"></v-text-field>
-          <v-text-field v-model="project.status" label="Project title"></v-text-field>
-          <v-text-field v-model="project.content" label="Project title"></v-text-field>
-          <v-text-field v-model="project.due" label="Project title"></v-text-field>
+          <v-text-field v-model="project.title" prepend-icon="folder" label="Project title"></v-text-field>
+
+          <v-select
+            :items="status"
+            required
+            v-model="project.chosenStatus"
+            prepend-icon="notifications"
+            label="Status"
+          ></v-select>
+          <v-text-field v-model="project.content" prepend-icon="edit" label="Project title"></v-text-field>
+          <v-menu>
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                :value="formattedDate"
+                v-on="on"
+                clearable
+                label="Due date"
+                prepend-icon="date_range"
+                :rules="inputRules"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="project.due"></v-date-picker>
+          </v-menu>
         </v-card-text>
       </v-card>
     </v-container>
@@ -16,20 +35,27 @@
 
 <script>
 import { db, fb } from "../firebase";
+import format from "date-fns/format";
 export default {
   data() {
     return {
       id: this.$route.params.id,
+      status: ["to-do", "urgent", "ongoing", "done"],
       project: {
         content: null,
         due: null,
         person: null,
         status: null,
-        title: null
+        title: null,
+        chosenStatus: ""
       },
       profile: {
         name: null
-      }
+      },
+      inputRules: [
+        v => !!v || "This field is required",
+        v => v.length >= 5 || "Minimum length is 5 characters"
+      ]
     };
   },
 
@@ -40,6 +66,11 @@ export default {
       project: db.collection("projects").doc(this.id)
     };
   },
-  methods: {}
+  methods: {},
+  computed: {
+    formattedDate() {
+      return this.project.due ? format(this.project.due, "YYYY-MM-DD") : "";
+    }
+  }
 };
 </script>
